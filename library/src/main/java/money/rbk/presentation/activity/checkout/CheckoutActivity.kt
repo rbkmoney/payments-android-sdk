84d37ed75834abcf3e4936187d9fd7acc757ffa5
Корчагин Артур Михайлович <artur.korchagin@simbirsoft.com>
2019-06-21 12:58:41 +0300

    Fixed backstack

--- library/src/main/java/money/rbk/presentation/activity/checkout/CheckoutActivity.kt
+++ library/src/main/java/money/rbk/presentation/activity/checkout/CheckoutActivity.kt
@@ -112,4 +112,8 @@ class CheckoutActivity : AppCompatActivity(), CheckoutView {
         lLoader.visibility = View.INVISIBLE
     }
 
+    override fun onBackPressed() {
+        navigator.back()
+    }
+
 }
