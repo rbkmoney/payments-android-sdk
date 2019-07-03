1344a35156870f92b680315c4117f1a5adea17a2
Arthur Korchagin <lliepmah@mail.ru>
2019-07-03 15:18:53 +0300

    Fixed application class

--- sample/src/main/java/money/rbk/sample/app/RBKSampleApplication.kt
+++ sample/src/main/java/money/rbk/sample/app/RBKSampleApplication.kt
@@ -1,13 +1,12 @@
 package money.rbk.sample.app
 
-import android.app.Application
 import money.rbk.sample.network.NetworkService
 
 /**
  * @author Arthur Korchagin (artur.korchagin@simbirsoft.com)
  * @since 24.06.19
  */
-class RBKSampleApplication : Application() {
+class RBKSampleApplication : RBKBaseApplication() {
 
     companion object {
         lateinit var networkService: NetworkService
