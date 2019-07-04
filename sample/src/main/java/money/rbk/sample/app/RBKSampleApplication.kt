a4ddccf9a269a7d8b9912a159450ac386bc0fda6
Корчагин Артур Михайлович <artur.korchagin@simbirsoft.com>
2019-07-04 17:48:40 +0300

    Fixed bug with wrong application class for release

--- sample/src/main/java/money/rbk/sample/app/RBKSampleApplication.kt
+++ sample/src/main/java/money/rbk/sample/app/RBKSampleApplication.kt
@@ -1,12 +1,13 @@
 package money.rbk.sample.app
 
+import android.app.Application
 import money.rbk.sample.network.NetworkService
 
 /**
  * @author Arthur Korchagin (artur.korchagin@simbirsoft.com)
  * @since 24.06.19
  */
-class RBKSampleApplication : RBKBaseApplication() {
+open class RBKSampleApplication : Application() {
 
     companion object {
         lateinit var networkService: NetworkService
